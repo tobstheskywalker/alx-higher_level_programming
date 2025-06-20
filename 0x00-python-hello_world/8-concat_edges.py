03b75dbdedd46976e33932e729163cca75bfeb7e
Tobstheskywalker <Tobiloba2569@gmail.com>
2025-06-19 21:39:10 -0700

    vi

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/Usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming"
-str3 = "with"
-str4 = "Python"
-print(str1 + str2 + str3 + str4) 
+str = "Python is interpreted, interactive, object-oriented programming\
+        language that combine remarkable power with very clear syntax" 
+str = str[39:67] + str[107:112] + str[:6]
+print(str) 
